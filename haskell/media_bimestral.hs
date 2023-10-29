e7c48a63ce953b790fa1a9b353518d2a9a3efccb
Lucas Lago <lukemartinslagonit@gmail.com>
2023-10-29 19:08:09 -0300

    added bimestral mean function

---
+++ haskell/media_bimestral.hs
@@ -0,0 +1,23 @@
+{- 
+Crie a função *med_bim* que recebe uma lista de números 
+e retorna as médias dos elementos dois a dois
+
+Exemplo:
+med_bim:: [Int] -> [Int]
+med_bim l = <...> foldr <...>
+-}
+
+f = (\atual (primeiro, acumulador) -> 
+    (not primeiro, 
+    if primeiro then 
+        acumulador 
+    else 
+        (atual + head acumulador) `div` 2 : acumulador))
+
+med_bim :: [Int] -> [Int]
+med_bim lista = snd $ foldr f (True, []) lista
+
+
+
+
+main = print(med_bim [7,9,3,7]) -- retorna [8,5]

5b9a26e43ebe285db145b735d0f5a7c1d3e6dacb
Lucas Lago <lukemartinslagonit@gmail.com>
2023-10-29 16:22:35 -0300

    update coment

--- haskell/inverte_lista.hs
+++ haskell/inverte_lista.hs
@@ -1,4 +1,4 @@
 inverte :: [tipo] -> [tipo] -- recebe e retorna uma lista de mesmo tipo
 inverte = foldr (\atual acumulador -> acumulador ++ [atual]) []
 
-main = print(inverte [[1,2], [3,4], [5,6]])
+main = print(inverte [[1,2], [3,4], [5,6]]) -- retorna [[5,6], [3,4], [1,2]]
